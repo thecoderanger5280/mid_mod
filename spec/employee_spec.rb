c3f5b4f1d717cac44ee017d90a06a75abce084c0
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-07 10:39:21 -0600

    Initial commit

---
+++ spec/employee_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/employee'
+
+RSpec.describe Employee do
+  describe '#Initialize' do
+    it 'exists' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+
+      expect(bobbi).to be_an_instance_of(Employee)
+    end
+
+    it 'has a name' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+      
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+    end
+
+    it 'has an age' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+
+      expect(bobbi.age).to eq(30)
+    end
+
+    it 'has a salary' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+      
+      expect(bobbi.salary).to eq(100000)
+    end
+  end
+end
