9571d97fc4f3bd6fa981e95bb5bb97980f4933ab
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-07 11:06:03 -0600

    implement code for expense method in department class

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,18 @@
 class Department
   attr_reader :name,
-              :employees
+              :employees,
+              :expenses
   def initialize(name)
     @name = name
     @employees = []
+    @expenses = 0
   end
 
   def hire(employee)
     @employees << employee
+  end
+
+  def expense(ammount)
+    @expenses += ammount
   end
 end
